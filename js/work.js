023615696a7cba3e1cb42864bb5070c8df48ff49
maganoegi <platserg94@yahoo.com>
2020-01-14 17:00:17 +0100

    pictures added

--- js/work.js
+++ js/work.js
@@ -1,8 +1,6 @@
 
 
 
-// Source: http://yonaba.github.io/2012/08/14/List-your-GitHub-projects-using-JavaScript-and-jQuery.md.html
-
 
 jQuery.githubUser = function(username, callback) {
     jQuery.getJSON('https://api.github.com/users/'+username+'/repos?callback=?',callback)
